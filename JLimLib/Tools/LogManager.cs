f4f896a25eb3f36ec3b7a626f25e7de9e5666fb5
jlim <jlim@wonik.com>
2024-06-08 13:40:35 +0900

    로그 확장 (Year-Month / Year 별)

--- JLimLib/Tools/LogManager.cs
+++ JLimLib/Tools/LogManager.cs
@@ -6,28 +6,47 @@ using System.Threading.Tasks;
 using System.IO;
 namespace JLimLib.Tools
 {
+    public enum LogType { Daily, Monthly }
+
     public class LogManager
     {
         private string _path;
 
         #region Constructors
-        public LogManager(string path)
+        public LogManager(string path, LogType logType)
         {
             _path = path;
-            _SetLogPath();
+            _SetLogPath(logType);
         }
-        public LogManager() : this(Path.Combine(Application.Root, "Log")) 
+        public LogManager() : this(Path.Combine(Application.Root, "Log"), LogType.Daily)
         { }
 
         #endregion
 
         #region Methods
-        private void _SetLogPath()
+        private void _SetLogPath(LogType logType)
         {
+            string path = String.Empty; // 로그파일이 저장될 루트경로의 중간 경로.
+            string name = String.Empty;
+
+            switch (logType)
+            {
+                case LogType.Daily:
+                    path = String.Format(@"{0}\{1}\", DateTime.Now.Year, DateTime.Now.ToString("MM"));
+                    name = DateTime.Now.ToString("yyyyMMdd") + ".txt";
+                    break;
+                case LogType.Monthly:
+                    path = String.Format(@"{0}\", DateTime.Now.Year);
+                    name = DateTime.Now.ToString("yyyyMM") + ".txt";
+                    break;
+            }
+
+            _path = Path.Combine(_path, path); // 컴바인 기능 확인.
+
             if (!Directory.Exists(_path))
                 Directory.CreateDirectory(_path);
-            string logFile = DateTime.Now.ToString("yyyyMMdd") + ".txt";
-            _path = Path.Combine(_path, logFile);
+
+            _path = Path.Combine(_path, name); // 파일 명까지 들어간 full 경로.
 
         }
         public void Write(string data)
